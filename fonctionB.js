318f552166562db6cade04a70df6293273964291
SamshaKimeri <alexandrariant@gmail.com>
2024-01-18 10:58:35 +0100

    functionExo

---
+++ fonctionB.js
@@ -0,0 +1,45 @@
+// function calculMoyenne(note1, note2, note3) {
+//   let moyenne = (note1 + note2 + note3) / 3;
+//   console.log(moyenne);
+// }
+// calculMoyenne(10, 2, 2);
+
+// function convertirCelsusEnFahreneit(temperatureCelsus) {
+//   let temperatureFahrenheit = (9 * temperatureCelsus) / 5 + 32;
+//   console.log(temperatureFahrenheit);
+// }
+// convertirCelsusEnFahreneit(10);
+
+// function vrifieParité(nombre) {
+//   if (nombre % 2 === 0) {
+//     console.log(`Le ${nombre} est pair`);
+//   } else {
+//     console.log(`Le ${nombre} est impair`);
+//   }
+// }
+// vrifieParité(10);
+
+// // Calculer une factorielle
+// function clculerFactorielle(nombre) {
+//   // Verification pour le cas où le nombre est 0 à 1
+//   // la factorielle est 1 dans ce cas
+//   if (nombre === 0 || nombre === 1) {
+//     console.log(`La factorielle de ${nombre} et 1 `);
+//   } else {
+//     let factorielle = 1;
+//     // Calcul de la factorielle
+//     for (let i = 2; i <= nombre; i++) {
+//       factorielle *= i;
+//     }
+//     // Affichage du résultat
+//     console.log(`La factorielle de ${nombre} est ${factorielle}`);
+//   }
+// }
+// // Appel de la fonction avec une valeur
+// clculerFactorielle(5);
+
+function sommeNombresPairs(limite) {
+  for (let i = 0; i < limite.length; i++) {}
+  console.log();
+}
+sommeNombresPairs(10);
